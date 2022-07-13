1ed7d2eae0f723ba1a6a317eb39bfe28f42ed3fb
jayeshkaushik2 <jayeshkaushik2@gmail.com>
2022-07-13 13:02:20 +0530

    fixed margins

--- src/components/ItemCart.js
+++ src/components/ItemCart.js
@@ -5,7 +5,8 @@ import CardContent from '@mui/material/CardContent';
 import CardMedia from '@mui/material/CardMedia';
 import Button from '@mui/material/Button';
 import Typography from '@mui/material/Typography';
-import background from '../assets/images/background.jpg' 
+import background from '../assets/images/background.jpg'
+
 
 export default function ItemCart() {
   return (
@@ -20,14 +21,10 @@ export default function ItemCart() {
         <Typography gutterBottom variant="h5" component="div">
           Lizard
         </Typography>
-        <Typography variant="body2" color="text.secondary">
-          Lizards are a widespread group of squamate reptiles, with over 6,000
-          species, ranging across all continents except Antarctica
-        </Typography>
       </CardContent>
       <CardActions>
-        <Button size="small">Share</Button>
-        <Button size="small">Learn More</Button>
+        <Button size="small" variant="outlined">Add to cart</Button>
+        <Button size="small" variant="outlined" sx={{marginLeft: 'auto'}}>View More</Button>
       </CardActions>
     </Card>
   );
