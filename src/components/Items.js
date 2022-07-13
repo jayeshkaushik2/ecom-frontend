584b7df5054ad1be97d85af372e0f2d8fdbe113b
jayeshkaushik2 <jayeshkaushik2@gmail.com>
2022-07-13 12:03:39 +0530

    added items, footer

---
+++ src/components/Items.js
@@ -0,0 +1,18 @@
+import React from 'react'
+import ItemCart from './ItemCart'
+import { Grid, } from '@mui/material';
+import Container from '@mui/material/Container';
+
+export const Items = () => {
+    return (
+        <Container maxWidth="xl" sx={{marginTop: '40px', marginBottom: '10px'}}>
+            <Grid container spacing={{ xs: 2, md: 3 }} columns={{ xs: 4, sm: 8, md: 12 }}>
+                {Array.from(Array(6)).map((_, index) => (
+                    <Grid item xs={2} sm={4} md={4} key={index}>
+                        <ItemCart />
+                    </Grid>
+                ))}
+            </Grid>
+        </Container>
+    )
+}
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,12 +1,16 @@
 import React from 'react'
 import { Header } from '../components/Header'
+import { Items } from '../components/Items'
 import Navbar from '../components/Navbar'
+import { Footer } from '../components/Footer'
 
 export const Home = () => {
     return (
         <>
             <Navbar />
             <Header />
+            <Items />
+            <Footer />
         </>
     )
 }
