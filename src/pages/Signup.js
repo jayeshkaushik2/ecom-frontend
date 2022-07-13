02cadec53b9006437bd6e88c5aefd3117d2accdc
Aman <amanrajput7055148434@gmail.com>
2022-07-13 13:01:48 +0530

    added signin background img

--- src/pages/Signup.js
+++ src/pages/Signup.js
@@ -1,8 +1,47 @@
-import React from 'react'
-import { TextField } from '@mui/material'
+import * as React from 'react';
+import Box from '@mui/material/Box';
+import TextField from '@mui/material/TextField';
+import Button from '@mui/material/Button';
+import signin_img from '../assets/images/signin_img.jpg'
+import { Container } from '@mui/system';
 
 export const Signup = () => {
   return (
-    <div><TextField id="outlined-basic" label="Outlined" variant="outlined" /></div>
-  )
+    <>
+      <Container maxWidth="xl"  sx={{backgroundImage:`url(${signin_img})`, height:"100vh"}}>
+
+
+        <Box
+          component="form"
+          sx={{
+            backgroundColor: "green",
+            display: "flex",
+            display: "grid",
+            justifyContent: "center",
+            '& .MuiTextField-root': { m: 1, width: '25ch' },
+          }}
+          noValidate
+          autoComplete="off"
+        >
+          <div>
+            <TextField
+              required
+              id="outlined-required"
+              label="Name"
+              defaultValue="Enter Name"
+            />
+            <TextField
+              id="outlined-password-input"
+              label="Password"
+              type="password"
+              autoComplete="current-password"
+            />
+          </div>
+          <Button variant="contained">Contained</Button>
+          <Button variant="text" sx={{ backgroundColor: "red", }}>Text</Button>
+
+        </Box>
+      </Container>
+    </>
+  );
 }
