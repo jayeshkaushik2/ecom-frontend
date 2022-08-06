0351d6f9951c619ecdb2212c9a94fa4d55e4d79d
jayeshkaushik2 <jayeshkaushik2@gmail.com>
2022-08-06 17:41:31 +0530

    fixed default page

--- src/pages/Default.js
+++ src/pages/Default.js
@@ -5,15 +5,16 @@ import Box from '@mui/material/Box';
 
 const Default = () => {
     return (
-        <Box style={{ backgorundColor: "#fff4e0", minHeight: "300px", marginTop: "40px", marginBottom: "40px", marginLeft: "10px", marginRight: "10px" }} id="default-page">
-
-            <Grid container spacing={{ xs: 2, md: 3 }} columns={{ xs: 2, sm: 8, md: 12 }}>
-                {Array.from(Array(3)).map((_, index) => (
-                    <Grid item xs={2} sm={4} md={4} key={index}>
-                        <DefaultSkelton />
-                    </Grid>
-                ))}
-            </Grid>
+        <Box minHeight="300px" sx={{ backgroundColor: "#fff4e0", paddingBottom: "10px", bottom: "0px" }}>
+            <Box sx={{ marginTop: '20px', marginBottom: '20px', maxWidth: "100%", flexGrow: 1 }} id="promoted-items">
+                <Grid container spacing={{ xs: 2, md: 3 }} columns={{ xs: 2, sm: 8, md: 12 }}>
+                    {Array.from(Array(3)).map((_, index) => (
+                        <Grid item xs={2} sm={4} md={4} key={index}>
+                            <DefaultSkelton />
+                        </Grid>
+                    ))}
+                </Grid>
+            </Box>
         </Box>
     )
 }
