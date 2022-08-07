0b8864b23f81129e83e78ab6e717063785d81e7d
jayeshkaushik2 <jayeshkaushik2@gmail.com>
2022-08-07 12:42:07 +0530

    fixes home header

--- src/components/HomeHeader.js
+++ src/components/HomeHeader.js
@@ -32,7 +32,7 @@ const HomeHeader = (props) => {
 
     const LeftbuttonStyle = {
         position: 'absolute',
-        top: '50%',
+        top: '40%',
         left: '10px',
         backgroundColor: main_color,
         '&:hover': {
@@ -42,7 +42,7 @@ const HomeHeader = (props) => {
 
     const RightbuttonStyle = {
         position: "absolute",
-        top: "50%",
+        top: "40%",
         right: "10px",
         backgroundColor: main_color,
         '&:hover': {
@@ -52,7 +52,7 @@ const HomeHeader = (props) => {
 
     return (
         <Box id="promoted-subcategory" minHeight="300px" sx={{ backgroundColor: "#fff4e0" }}>
-            <Box sx={{ position: "absolute", top: "50%", left: "50%", transform: "translate(-50%, -50%)", color: "white" }}>
+            <Box sx={{ position: "absolute", top: "40%", left: "50%", transform: "translate(-50%, -50%)", color: "white" }}>
                 <Typography variant="h5" sx={{ textAlign: "center" }}>
                     {HomepageData ? HomepageData["title"] : "Title"}
                 </Typography>
