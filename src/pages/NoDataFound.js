cc322979edf0fb02debfcfe0b1390f9e2cce3885
jayeshkaushik2 <jayeshkaushik2@gmail.com>
2022-08-20 21:33:30 +0530

    fixed layout

--- src/pages/NoDataFound.js
+++ src/pages/NoDataFound.js
@@ -3,10 +3,9 @@ import Box from '@mui/material/Box';
 import No_Data_Found from '../assets/images/no_data_found.jpg'
 
 const NoDataFound = () => {
-  console.log("new 1")
   return (
     <Box>
-      <img width="100%" src={No_Data_Found} alt="no data found" />
+      <img width="fit-content" src={No_Data_Found} alt="no data found" />
     </Box>
   )
 }
