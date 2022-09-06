06054c876c82d8f5e128d2713961b1071a5cfc3f
jayeshkaushik2 <jayeshkaushik2@gmail.com>
2022-09-06 22:16:16 +0530

    fixed imports

--- src/components/OrderLine.js
+++ src/components/OrderLine.js
@@ -1,5 +1,4 @@
 import * as React from 'react';
-import Card from '@mui/material/Card';
 import CardContent from '@mui/material/CardContent';
 import CardMedia from '@mui/material/CardMedia';
 import Typography from '@mui/material/Typography';
