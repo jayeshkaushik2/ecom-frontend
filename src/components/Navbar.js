8573b1ad0ba26907c63378def25b9e18850e2a4c
jayeshkaushik2 <jayeshkaushik2@gmail.com>
2022-12-12 20:57:37 +0530

    feature: added filtering in product list page.

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,211 +1,240 @@
-import * as React from 'react';
-import AppBar from '@mui/material/AppBar';
-import Box from '@mui/material/Box';
-import Toolbar from '@mui/material/Toolbar';
-import IconButton from '@mui/material/IconButton';
-import Typography from '@mui/material/Typography';
-import Menu from '@mui/material/Menu';
-import Avatar from '@mui/material/Avatar';
-import Tooltip from '@mui/material/Tooltip';
-import MenuItem from '@mui/material/MenuItem';
-import AdbIcon from '@mui/icons-material/Adb';
-import SearchIcon from '@mui/icons-material/Search';
-import { styled, alpha } from '@mui/material/styles';
-import InputBase from '@mui/material/InputBase';
-import AuthContext from '../context/AuthContext';
-import Link from '@mui/material/Link';
-import { useNavigate } from 'react-router-dom';
-import { getProfileData } from '../context/Apis'
+import * as React from "react";
+import AppBar from "@mui/material/AppBar";
+import Box from "@mui/material/Box";
+import Toolbar from "@mui/material/Toolbar";
+import IconButton from "@mui/material/IconButton";
+import Typography from "@mui/material/Typography";
+import Menu from "@mui/material/Menu";
+import Avatar from "@mui/material/Avatar";
+import Tooltip from "@mui/material/Tooltip";
+import MenuItem from "@mui/material/MenuItem";
+import AdbIcon from "@mui/icons-material/Adb";
+import SearchIcon from "@mui/icons-material/Search";
+import { styled, alpha } from "@mui/material/styles";
+import InputBase from "@mui/material/InputBase";
+import AuthContext from "../context/AuthContext";
+import Link from "@mui/material/Link";
+import { useNavigate } from "react-router-dom";
+import { getProfileData } from "../context/Apis";
 
 const ResponsiveAppBar = (props) => {
-    const [anchorElUser, setAnchorElUser] = React.useState(null);
-    let redirect = useNavigate()
-    let userData = React.useContext(AuthContext)
-    let API_ENDPOINT = process.env.REACT_APP_API_ENDPOINT
-    const [ProfileuserData, setProfileuserData] = React.useState(null)
-    const [Query, setQuery] = React.useState("")
+  const [anchorElUser, setAnchorElUser] = React.useState(null);
+  let redirect = useNavigate();
+  let userData = React.useContext(AuthContext);
+  let API_ENDPOINT = process.env.REACT_APP_API_ENDPOINT;
+  const [ProfileuserData, setProfileuserData] = React.useState(null);
+  const [Query, setQuery] = React.useState("");
 
-    const getData = async () => {
-        try {
-            let token = userData.AuthToken ? `Bearer ${userData.AuthToken.access}` : null
-            if (token !== null && userData.Login !== "Login" && userData.AuthToken !== null) {
-                const data = await getProfileData({ token: token })
-                setProfileuserData(data)
-            }
-        }
-        catch (error) {
-            console.log(error)
-        }
+  const getData = async () => {
+    try {
+      let token = userData.AuthToken
+        ? `Bearer ${userData.AuthToken.access}`
+        : null;
+      if (
+        token !== null &&
+        userData.Login !== "Login" &&
+        userData.AuthToken !== null
+      ) {
+        const data = await getProfileData({ token: token });
+        setProfileuserData(data);
+      }
+    } catch (error) {
+      console.log(error);
     }
+  };
 
-    React.useEffect(() => {
-        console.log("query", Query)
-        getData()
-    }, [])
+  React.useEffect(() => {
+    console.log("query", Query);
+    getData();
+  }, []);
 
-    const handleSearchChange = (e) => {
-        let query = e.target.value
-        console.log("runing fuc...s", query)
-        redirect("/products", { state: { query: query } })
-    }
+  const handleSearchChange = (e) => {
+    let query = e.target.value;
+    redirect("/products", { state: { query: { sub_category__name: query } } });
+  };
 
+  const handleOpenUserMenu = (event) => {
+    setAnchorElUser(event.currentTarget);
+  };
 
-    const handleOpenUserMenu = (event) => {
-        setAnchorElUser(event.currentTarget);
-    };
+  const handleCloseUserMenu = () => {
+    setAnchorElUser(null);
+  };
 
-    const handleCloseUserMenu = () => {
-        setAnchorElUser(null);
-    };
+  const handleLogout = () => {
+    userData.logoutUser();
+  };
 
-    const handleLogout = () => {
-        userData.logoutUser()
-    }
+  const Search = styled("div")(({ theme }) => ({
+    position: "relative",
+    borderRadius: theme.shape.borderRadius,
+    backgroundColor: alpha(theme.palette.common.white, 0.15),
+    "&:hover": {
+      backgroundColor: alpha(theme.palette.common.white, 0.25),
+    },
+    width: "100%",
+    [theme.breakpoints.up("sm")]: {
+      marginLeft: theme.spacing(1),
+      width: "auto",
+    },
+  }));
 
-    const Search = styled('div')(({ theme }) => ({
-        position: 'relative',
-        borderRadius: theme.shape.borderRadius,
-        backgroundColor: alpha(theme.palette.common.white, 0.15),
-        '&:hover': {
-            backgroundColor: alpha(theme.palette.common.white, 0.25),
+  const SearchIconWrapper = styled("div")(({ theme }) => ({
+    padding: theme.spacing(0, 2),
+    height: "100%",
+    position: "absolute",
+    pointerEvents: "none",
+    display: "flex",
+    alignItems: "center",
+    justifyContent: "center",
+  }));
+
+  const StyledInputBase = styled(InputBase)(({ theme }) => ({
+    color: "inherit",
+    "& .MuiInputBase-input": {
+      padding: theme.spacing(1, 1, 1, 0),
+      // vertical padding + font size from searchIcon
+      paddingLeft: `calc(1em + ${theme.spacing(4)})`,
+      transition: theme.transitions.create("width"),
+      width: "100%",
+      [theme.breakpoints.up("sm")]: {
+        width: "50ch",
+        "&:focus": {
+          width: "50ch",
         },
-        width: '100%',
-        [theme.breakpoints.up('sm')]: {
-            marginLeft: theme.spacing(1),
-            width: 'auto',
-        },
-    }));
+      },
+    },
+  }));
 
-    const SearchIconWrapper = styled('div')(({ theme }) => ({
-        padding: theme.spacing(0, 2),
-        height: '100%',
-        position: 'absolute',
-        pointerEvents: 'none',
-        display: 'flex',
-        alignItems: 'center',
-        justifyContent: 'center',
-    }));
+  return (
+    <AppBar
+      position="static"
+      sx={{
+        position: "sticky",
+        top: 0,
+        zIndex: "100",
+        background: "#1769aa",
+        maxWidth: "100%",
+      }}
+    >
+      <Box sx={{ paddingLeft: "10px", paddingRight: "10px" }}>
+        <Toolbar disableGutters>
+          <Typography
+            variant="h6"
+            noWrap
+            component="a"
+            href="/"
+            sx={{
+              mr: 2,
+              display: { xs: "none", md: "flex" },
+              fontFamily: "monospace",
+              fontWeight: 700,
+              letterSpacing: ".3rem",
+              color: "inherit",
+              textDecoration: "none",
+            }}
+          >
+            EcomBUY
+          </Typography>
 
-    const StyledInputBase = styled(InputBase)(({ theme }) => ({
-        color: 'inherit',
-        '& .MuiInputBase-input': {
-            padding: theme.spacing(1, 1, 1, 0),
-            // vertical padding + font size from searchIcon
-            paddingLeft: `calc(1em + ${theme.spacing(4)})`,
-            transition: theme.transitions.create('width'),
-            width: '100%',
-            [theme.breakpoints.up('sm')]: {
-                width: '50ch',
-                '&:focus': {
-                    width: '50ch',
-                },
-            },
-        },
-    }));
+          <AdbIcon sx={{ display: { xs: "flex", md: "none" }, mr: 1 }} />
+          <Typography
+            variant="h5"
+            noWrap
+            component="a"
+            href="/"
+            sx={{
+              mr: 2,
+              display: { xs: "flex", md: "none" },
+              flexGrow: 1,
+              fontFamily: "monospace",
+              fontWeight: 700,
+              letterSpacing: ".3rem",
+              color: "inherit",
+              textDecoration: "none",
+            }}
+          >
+            EcomBUY
+          </Typography>
 
-    return (
-        <AppBar position="static" sx={{ position: "sticky", top: 0, zIndex: "100", background:"#1769aa", maxWidth: "100%" }}>
-            <Box sx={{ paddingLeft: "10px", paddingRight: "10px" }}>
-                <Toolbar disableGutters>
-                    <Typography
-                        variant="h6"
-                        noWrap
-                        component="a"
-                        href="/"
-                        sx={{
-                            mr: 2,
-                            display: { xs: 'none', md: 'flex' },
-                            fontFamily: 'monospace',
-                            fontWeight: 700,
-                            letterSpacing: '.3rem',
-                            color: 'inherit',
-                            textDecoration: 'none',
-                        }}
-                    >
-                        EcomBUY
-                    </Typography>
+          <Box
+            sx={{
+              mr: "10px",
+              ml: "100px",
+              flexGrow: 1,
+              display: { xs: "none", md: "flex" },
+              marginLeft: "auto",
+            }}
+          >
+            <Search>
+              <SearchIconWrapper>
+                <SearchIcon />
+              </SearchIconWrapper>
+              <StyledInputBase
+                onKeyPress={(e) => {
+                  if (e.key === "Enter") {
+                    handleSearchChange(e);
+                  }
+                }}
+                placeholder="Search your product"
+                inputProps={{ "aria-label": "search" }}
+              />
+            </Search>
+          </Box>
+          <Box sx={{ flexGrow: 0 }}>
+            <Tooltip title="Open settings">
+              <IconButton onClick={handleOpenUserMenu} sx={{ p: 0 }}>
+                <Avatar
+                  src={
+                    ProfileuserData?.profile_image
+                      ? `${API_ENDPOINT}${ProfileuserData.profile_image}`
+                      : "/static/images/avatar/1.jpg"
+                  }
+                />
+              </IconButton>
+            </Tooltip>
+            <Menu
+              sx={{ mt: "45px" }}
+              id="menu-appbar"
+              anchorEl={anchorElUser}
+              anchorOrigin={{
+                vertical: "top",
+                horizontal: "right",
+              }}
+              keepMounted
+              transformOrigin={{
+                vertical: "top",
+                horizontal: "right",
+              }}
+              open={Boolean(anchorElUser)}
+              onClose={handleCloseUserMenu}
+            >
+              {userData?.user ? (
+                <MenuItem onClick={() => handleCloseUserMenu()}>
+                  <Link href="/profile" underline="none" color="inherit">
+                    <Typography textAlign="center">Profile</Typography>
+                  </Link>
+                </MenuItem>
+              ) : null}
 
-                    <AdbIcon sx={{ display: { xs: 'flex', md: 'none' }, mr: 1 }} />
-                    <Typography
-                        variant="h5"
-                        noWrap
-                        component="a"
-                        href="/"
-                        sx={{
-                            mr: 2,
-                            display: { xs: 'flex', md: 'none' },
-                            flexGrow: 1,
-                            fontFamily: 'monospace',
-                            fontWeight: 700,
-                            letterSpacing: '.3rem',
-                            color: 'inherit',
-                            textDecoration: 'none',
-                        }}
-                    >
-                        EcomBUY
-                    </Typography>
-
-                    <Box sx={{ mr: '10px', ml: '100px', flexGrow: 1, display: { xs: 'none', md: 'flex' }, marginLeft: "auto" }}>
-                        <Search>
-                            <SearchIconWrapper>
-                                <SearchIcon />
-                            </SearchIconWrapper>
-                            <StyledInputBase
-                                onKeyPress={(e) => { if (e.key === "Enter") { handleSearchChange(e) } }}
-                                placeholder="Search your product"
-                                inputProps={{ 'aria-label': 'search' }}
-                            />
-                        </Search>
-                    </Box>
-                    <Box sx={{ flexGrow: 0 }}>
-                        <Tooltip title="Open settings">
-                            <IconButton onClick={handleOpenUserMenu} sx={{ p: 0 }}>
-                                <Avatar
-                                    src={ProfileuserData?.profile_image ? `${API_ENDPOINT}${ProfileuserData.profile_image}` : "/static/images/avatar/1.jpg"} />
-                            </IconButton>
-                        </Tooltip>
-                        <Menu
-                            sx={{ mt: '45px' }}
-                            id="menu-appbar"
-                            anchorEl={anchorElUser}
-                            anchorOrigin={{
-                                vertical: 'top',
-                                horizontal: 'right',
-                            }}
-                            keepMounted
-                            transformOrigin={{
-                                vertical: 'top',
-                                horizontal: 'right',
-                            }}
-                            open={Boolean(anchorElUser)}
-                            onClose={handleCloseUserMenu}
-                        >
-                            {userData?.user ?
-                                <MenuItem onClick={() => handleCloseUserMenu()}>
-                                    <Link href="/profile" underline="none" color="inherit">
-                                        <Typography textAlign="center">Profile</Typography>
-                                    </Link>
-                                </MenuItem>
-                                : null}
-
-                            {userData?.login === "Login" && userData.user === null ?
-                                <MenuItem onClick={() => handleCloseUserMenu()}>
-                                    <Link href="/signin" underline="none" color="inherit">
-                                        <Typography textAlign="center">Login</Typography>
-                                    </Link>
-                                </MenuItem>
-                                :
-
-                                <MenuItem onClick={() => handleCloseUserMenu()}>
-                                    <Link onClick={handleLogout} underline="none" color="inherit">
-                                        <Typography textAlign="center">Logout</Typography>
-                                    </Link>
-                                </MenuItem>}
-                        </Menu>
-                    </Box>
-                </Toolbar>
-            </Box>
-        </AppBar>
-    );
+              {userData?.login === "Login" && userData.user === null ? (
+                <MenuItem onClick={() => handleCloseUserMenu()}>
+                  <Link href="/signin" underline="none" color="inherit">
+                    <Typography textAlign="center">Login</Typography>
+                  </Link>
+                </MenuItem>
+              ) : (
+                <MenuItem onClick={() => handleCloseUserMenu()}>
+                  <Link onClick={handleLogout} underline="none" color="inherit">
+                    <Typography textAlign="center">Logout</Typography>
+                  </Link>
+                </MenuItem>
+              )}
+            </Menu>
+          </Box>
+        </Toolbar>
+      </Box>
+    </AppBar>
+  );
 };
 export default ResponsiveAppBar;
--- src/pages/ProductList.js
+++ src/pages/ProductList.js
@@ -1,56 +1,72 @@
-import React from 'react'
-import ProductView from '../components/ProductView'
-import { Box, Typography } from '@mui/material';
-import { getProductData_WithFilter } from '../context/Apis'
-import InputLabel from '@mui/material/InputLabel';
-import MenuItem from '@mui/material/MenuItem';
-import FormControl from '@mui/material/FormControl';
-import Select from '@mui/material/Select';
-import NoDataFound from './NoDataFound';
-import { useLocation } from 'react-router-dom';
+import React from "react";
+import ProductView from "../components/ProductView";
+import { Box, Button, Typography } from "@mui/material";
+import { getProductData } from "../context/Apis";
+import InputLabel from "@mui/material/InputLabel";
+import MenuItem from "@mui/material/MenuItem";
+import FormControl from "@mui/material/FormControl";
+import Select from "@mui/material/Select";
+import NoDataFound from "./NoDataFound";
+import { useLocation } from "react-router-dom";
 
 export const ProductList = (props) => {
-  const [ProductsData, setProductsData] = React.useState(null)
-  const [PriceRange, setPriceRange] = React.useState('');
-  const [Rating, setRating] = React.useState('');
-  const [Discount, setDiscount] = React.useState('');
-  const [Prev_query, setPrev_query] = React.useState('');
+  const [ProductsData, setProductsData] = React.useState(null);
+  const [PriceRange, setPriceRange] = React.useState("");
+  const [Rating, setRating] = React.useState("");
+  const [Discount, setDiscount] = React.useState("");
+  const [Prev_query, setPrev_query] = React.useState("");
   const [Search, setSearch] = React.useState("");
-  let location = useLocation()
+  const [filters, setFilters] = React.useState({});
+  let location = useLocation();
 
-
-  const handlePriceFilterChange = (event) => {
-    setPriceRange(event.target.value);
-  };
-
-  const handleRatingFilterChange = (event) => {
-    setRating(event.target.value);
-  };
-
-  const handleDiscountFilterChange = (event) => {
-    setDiscount(event.target.value);
-  };
-
-  const getData = async (query) => {
-    try {
-      const data = await getProductData_WithFilter({ search_with: "sub_category__name", query })
-      setProductsData(data["results"])
+  function update_filters(data) {
+    let temp = filters;
+    for (var item in data) {
+      temp[item] = data[item];
     }
-    catch (error) {
-      console.log(error)
-    }
+    setFilters(temp);
   }
+
+  const handlePriceFilterChange = (e) => {
+    setPriceRange(e.target.value);
+    let temp = e.target.value.split("-");
+    let gt = temp[0];
+    let lt = temp[1];
+    let filter = {};
+    if (lt !== null && lt !== undefined) {
+      filter["price__lt"] = lt;
+    }
+    if (gt !== null && gt !== undefined) {
+      filter["price__gt"] = gt;
+    }
+    update_filters(filter);
+    getFilterData(filter);
+  };
+
+  const handleRatingFilterChange = (e) => {
+    setRating(e.target.value);
+    update_filters({ rating: e.target.value });
+  };
+
+  const handleDiscountFilterChange = (e) => {
+    setDiscount(e.target.value);
+    update_filters({ discount__gte: e.target.value });
+  };
+
+  const getFilterData = async (query_params) => {
+    try {
+      const data = await getProductData({ filters: query_params });
+      setProductsData(data["results"]);
+    } catch (error) {
+      console.log(error);
+    }
+  };
 
   React.useEffect(() => {
-    const query = location?.state.query
-    getData(query)
-  }, [location?.state])
-  
-
-  if (props.Query !== Prev_query) {
-    setPrev_query(props.Query)
-    getData()
-  }
+    const query = location?.state.query;
+    update_filters(query);
+    getFilterData(query);
+  }, [location?.state]);
 
   const filterStyle = {
     minWidth: "20%",
@@ -64,14 +80,20 @@ export const ProductList = (props) => {
     height: "fit-content",
     overflow: "auto",
     maxHeight: "580px",
-  }
+  };
 
   return (
-    <Box sx={{ display: "flex", padding: "15px", marginBottom: "-40px", minHeight: "300px", maxWidth: "100%" }}>
+    <Box
+      sx={{
+        display: "flex",
+        padding: "15px",
+        marginBottom: "-40px",
+        minHeight: "300px",
+        maxWidth: "100%",
+      }}
+    >
       <Box sx={filterStyle}>
-        <Typography variant="h6">
-          Filters
-        </Typography>
+        <Typography variant="h6">Filters</Typography>
         <hr />
         <Box sx={{ minWidth: 120 }}>
           <Box marginBottom="10px">
@@ -120,29 +142,47 @@ export const ProductList = (props) => {
                 label="Discount"
                 onChange={handleDiscountFilterChange}
               >
-                <MenuItem value={0.1}>10% +</MenuItem>
-                <MenuItem value={0.2}>20% +</MenuItem>
-                <MenuItem value={0.3}>30% +</MenuItem>
+                <MenuItem value={10}>10% +</MenuItem>
+                <MenuItem value={20}>20% +</MenuItem>
+                <MenuItem value={30}>30% +</MenuItem>
               </Select>
             </FormControl>
           </Box>
+          <Button
+            variant="contained"
+            sx={{ width: "100%" }}
+            onClick={(e) => {
+              setFilters({});
+            }}
+          >
+            Clear filters
+          </Button>
         </Box>
-
-      </Box >
+      </Box>
 
       <Box sx={{ padding: "15px", marginLeft: "15px", width: "100%" }}>
-        {ProductsData?.length > 0 ? ProductsData.map((data, index) => (
-          <ProductView setShowMsg={props.setShowMsg} key={index} productData={data} PriceRange={PriceRange} getNumProduct={props.getNumProduct} />
-        )) :
-          <Box sx={{
-            maxWidth: "100%",
-            maxHeight: "270px",
-            overflow: "hidden"
-          }}>
+        {ProductsData?.length > 0 ? (
+          ProductsData.map((data, index) => (
+            <ProductView
+              setShowMsg={props.setShowMsg}
+              key={index}
+              productData={data}
+              PriceRange={PriceRange}
+              getNumProduct={props.getNumProduct}
+            />
+          ))
+        ) : (
+          <Box
+            sx={{
+              maxWidth: "100%",
+              maxHeight: "270px",
+              overflow: "hidden",
+            }}
+          >
             <NoDataFound />
           </Box>
-        }
+        )}
       </Box>
-    </Box >
-  )
-}
+    </Box>
+  );
+};
